a49e576c8028071cd9234fa04a400558746b40c3
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-10-29 17:21:56 +0900

    Fix

--- field_set.py
+++ field_set.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from multiprocessing.sharedctypes import Value
+import re
 
 
 @dataclass
-class FieldSet:
+class FieldElement:
     num: int
     prime: int
 
@@ -11,7 +13,17 @@ class FieldSet:
         if self.num < 0 or self.num >= self.prime:
             raise ValueError
 
-    def __eq__(self, other: object) ->  bool:
-        if not isinstance(other, FieldSet):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, FieldElement):
             return False
         return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+    def __add__(self, other: object) -> FieldElement:
+        if not isinstance(other, FieldElement):
+            raise TypeError
+        if self.prime != other.prime:
+            raise TypeError
+        return FieldElement((self.num + other.num) % self.prime, self.prime)
--- tests/test_field_set.py
+++ tests/test_field_set.py
@@ -1,13 +1,33 @@
 import pytest
-from field_set import FieldSet
+from field_set import FieldElement
 
 
-class TestField:
-    @pytest.mark.parametrize("lhs,rhs,expected", [
-        ((1, 3), (1,2), False),
-        ((1, 3), (1,3), True),
-        ((1, 3), (2,3), False),
-    ])
-    def test_eq(self, lhs: tuple[int,int], rhs: tuple[int,int], expected: bool) -> None:
-        actual = FieldSet(*lhs) == FieldSet(*rhs)
+class TestFieldElement:
+    @pytest.mark.parametrize(
+        "lhs,rhs,expected",
+        [
+            ((1, 3), (1, 2), False),
+            ((1, 3), (1, 3), True),
+            ((1, 3), (2, 3), False),
+        ],
+    )
+    def test_eq(
+        self, lhs: tuple[int, int], rhs: tuple[int, int], expected: bool
+    ) -> None:
+        actual = FieldElement(*lhs) == FieldElement(*rhs)
         assert actual == expected
+
+    @pytest.mark.parametrize(
+        "lhs,rhs,expected",
+        [
+            (
+                FieldElement(1, 2),
+                FieldElement(1, 2),
+                FieldElement(0, 2),
+            ),
+        ],
+    )
+    def test_add(
+        self, lhs: FieldElement, rhs: FieldElement, expected: FieldElement
+    ) -> None:
+        assert lhs + rhs == expected
