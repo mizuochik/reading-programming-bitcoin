ef99606defe9c8d809ac421e964708f8572e099a
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-11-13 21:46:03 +0900

    Fix

--- point.py
+++ point.py
@@ -5,10 +5,10 @@ from typing import Optional
 
 @dataclass
 class Point:
-    x: Optional[int]
-    y: Optional[int]
-    a: int
-    b: int
+    x: Optional[float]
+    y: Optional[float]
+    a: float
+    b: float
 
     def __post_init__(self) -> None:
         if self.x is None and self.y is None:
@@ -18,8 +18,7 @@ class Point:
         assert self.y**2 == self.x**3 + self.a * self.x + self.b
 
     def __eq__(self, other: object):
-        if not isinstance(other, Point):
-            raise TypeError
+        assert isinstance(other, Point)
         return (
             self.a == other.a
             and self.b == other.b
@@ -31,15 +30,18 @@ class Point:
         return not (self == other)
 
     def __add__(self, other: object) -> Point:
-        if not isinstance(other, Point):
-            raise TypeError
+        assert isinstance(other, Point)
         if self.a != other.a or self.b != other.b:
             raise TypeError
         if self.x is None:
             return other
         if other.x is None:
             return self
+        assert self.y is not None
+        assert other.y is not None
         if self.x == other.x:
             return Point(None, None, self.a, self.b)
-        else:
-            raise NotImplementedError
+        s = (other.y - self.y) / (other.x - self.x)
+        x = s**2 - self.x - other.x
+        y = s * (self.x - x) - self.y
+        return Point(x, y, self.a, self.b)
