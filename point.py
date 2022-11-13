4fee396c4a5f9148791bd39547b8ad0b12c69b30
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-11-13 21:26:19 +0900

    Fix

--- point.py
+++ point.py
@@ -1,16 +1,21 @@
+from __future__ import annotations
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Point:
-    x: int
-    y: int
+    x: Optional[int]
+    y: Optional[int]
     a: int
     b: int
 
     def __post_init__(self) -> None:
-        if self.y**2 != self.x**3 + self.a * self.x + self.b:
-            raise ValueError
+        if self.x is None and self.y is None:
+            return
+        assert self.x is not None
+        assert self.y is not None
+        assert self.y**2 == self.x**3 + self.a * self.x + self.b
 
     def __eq__(self, other: object):
         if not isinstance(other, Point):
@@ -24,3 +29,17 @@ class Point:
 
     def __ne__(self, other: object):
         return not (self == other)
+
+    def __add__(self, other: object) -> Point:
+        if not isinstance(other, Point):
+            raise TypeError
+        if self.a != other.a or self.b != other.b:
+            raise TypeError
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if self.x == other.x:
+            return Point(None, None, self.a, self.b)
+        else:
+            raise NotImplementedError
